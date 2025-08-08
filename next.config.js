8a1e59ea848d8c98d5690c54acbf3e3c4bcff2c1
Виталий <mulikavj@gmail.com>
2025-08-08 16:24:25 +0300

    Add next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['placeholder.com'],
   },
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
-  },
+  }
 }
 
 module.exports = nextConfig
